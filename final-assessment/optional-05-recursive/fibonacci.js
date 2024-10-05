9900f5e44e07f7f1f32220d2fd3a49890974123b
unknown <19623007@std.stei.itb.ac.id>
2024-10-05 11:35:35 +0700

    task 5

--- final-assessment/optional-05-recursive/fibonacci.js
+++ final-assessment/optional-05-recursive/fibonacci.js
@@ -1,5 +1,12 @@
 function fibonacci(n) {
-
+    
+    if (n <= 0){
+        return [0];
+    }else if (n == 1){
+        return [0,1];
+    }
+    let arr = [];
+    return arr.concat(fibonacci(n-1),[fibonacci(n-2)[n-2] + fibonacci(n-1)[n-1]]);
 }
 
 // Jangan hapus kode di bawah ini!
