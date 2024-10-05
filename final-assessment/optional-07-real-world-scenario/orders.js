719e75fc971518e03635991bcb9092e52c987c80
unknown <19623007@std.stei.itb.ac.id>
2024-10-05 13:38:00 +0700

    task 7

--- final-assessment/optional-07-real-world-scenario/orders.js
+++ final-assessment/optional-07-real-world-scenario/orders.js
@@ -5,18 +5,36 @@ function generateUniqueId() {
 
 
 // TODO: buatlah variabel yang menampung data orders
-let orders;
+let orders = [];
 
 // TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+function addOrder(customerName, items) {
+  const detail = {
+    id: generateUniqueId(),
+    customerName: customerName,
+    items: items,
+    totalPrice: items.reduce((acc,item) => acc + item.price,0),
+    status: 'Menunggu'
+  }
+  orders.push(detail);
+}
 
 // TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+function updateOrderStatus(orderId, status) {
+  orders = orders.map((detail) =>{
+    return {...detail, status: detail.id === orderId ? status : detail.status}
+  });
+}
 
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+function calculateTotalRevenue() {
+  return orders.reduce((acc,detail) => 
+    detail.status === "Selesai" ? acc + detail.totalPrice : acc ,0);
+}
 
 // TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+function deleteOrder(id) {
+  orders = orders.filter( (item) => item.id !== id);
+}
 
 export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
